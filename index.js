5b13c07d048dbf563ec3a4d56ba6c6e88fc73cb1
geraldos <sepdwijaya18@gmail.com>
2020-06-19 16:09:34 +0700

    update value

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ buttonValue.addEventListener('click', function () {
     let inputValue = document.getElementById('input').value
 
     if (inputValue.match(regexNumber)) {
-        alert('value anda benar')
+        alert('Nomor yang anda masukan benar')
     } else {
-        alert('value anda salah')
+        alert('value harus berisi angka / angka anda kurang dari 10 angka atau lebih dari 12 angka')
     }
 })
